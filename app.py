34fbcd731d27544b56fb52718e3d2d418bde7ab5
Upsham Naik <upshamgnaik@outlook.com>
2020-04-10 23:56:12 -0400

    added dice roll

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 r = ["r1.png","r2.png","r3.png","r4.png","r5.png","r6.png","r7.png","r8.png","r9.png","r10.png","r11.png","r12.png","r13.png","r14.png","r15.png","r16.png","r17.png","r18.png","r19.png","r20.png"]
 m = ["m1.png","m2.png","m3.png","m4.png","m5.png","m6.png","m7.png","m8.png","m9.png","m10.png","m11.png","m12.png","m13.png","m14.png","m15.png","m16.png","m17.png","m18.png","m19.png","m20.png"]
+d = ["1.png","2.png","3.png","4.png","5.png","6.png"]
 
 
 @app.route("/")
@@ -12,13 +13,15 @@ def index():
 
 @app.route("/pickr")
 def pickr():
-    f = r[random.randint(0,19)]
-    return render_template("result.html", file = f)
+    return render_template("result.html", file = r[random.randint(0,19)])
 
 @app.route("/pickm")
 def pickm():
-    f = m[random.randint(0,19)]
-    return render_template("result.html", file = f)
+    return render_template("result.html", file = m[random.randint(0,19)])
+
+@app.route("/roll")
+def roll():
+    return render_template("index.html", die = d[random.randint(0,5)])
 
 if __name__ == "__main__":
     app.run(debug=True)
